095a4015c0d6ecca18e3a330b462b829bf64b334
sajedul2022 <sajedul.idb.info@gmail.com>
2022-12-15 23:35:11 +0600

    Inventory: crud- read

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <div class="mt-5 p-4 bg-dark text-white text-center">
+        <p>Copyright (c) All rights reserved 2022 By Sajedul WareHouse</p>
+      </div>
+    </div>
+  );
+}
---
+++ src/Components/Header.js
@@ -0,0 +1,80 @@
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+export default function Header() {
+  let navigate = useNavigate();
+  // session clear
+  const logout = () => {
+    sessionStorage.clear();
+    navigate("/login");
+  };
+
+  // Not entry Dashboard
+  let fullname = sessionStorage.getItem("fullname");
+  let setEmail = sessionStorage.getItem("email");
+  // useEffect(() => {
+  //   if (setEmail == null) {
+  //     navigate("/login");
+  //   }
+  // }, []);
+
+  return (
+    <div class="container">
+      <div class="row">
+        <div class="p-5 jamb bg-primary  text-center">
+          <p>Stock Management System!</p>
+        </div>
+
+        <nav class="navbar navbar-expand-sm ">
+        
+          <Link class="navbar-brand" to="/"><img src="/assets/image/logo.png"/></Link>
+    
+              
+            
+          <div class="container-fluid">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <Link class="nav-link active" to="/home">
+                  Home
+                </Link>
+              </li>
+
+              <li class="nav-item">
+                <Link class="nav-link" to="/products">
+                  All Product
+                </Link>
+              </li>
+
+              <li class="nav-item">
+                <Link class="nav-link" to="/registration">
+                  Registration
+                </Link>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link active"
+                  href="http://localhost/react_resource/react-inventory/Inventory-Management_php">
+                    Admin
+                </a>
+              </li>
+
+              <span></span>
+            </ul>
+            <span className="navbar-text"> {setEmail} </span>
+            <span>
+              {fullname ? (
+                <a onClick={logout} class="nav-link">
+                  {fullname} | Logout
+                </a>
+              ) : (
+                <Link to="/login" class="nav-link">
+                  Login
+                </Link>
+              )}
+            </span>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+}
